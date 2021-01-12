1e8a884487e03713ece2dc0d16f00595868325d9
Nathan-Guillaume <45270643+Nathan-Guillaume@users.noreply.github.com>
2021-01-12 16:03:52 +0100

    Add other scripts

---
+++ discord/discord_bot_install.sh
@@ -0,0 +1,21 @@
+#  _   _       _   _                  _____           _       _       
+# | \ | |     | | | |                / ____|         (_)     | |      
+# |  \| | __ _| |_| |__   __ _ _ __ | (___   ___ _ __ _ _ __ | |_ ___ 
+# | . ` |/ _` | __| '_ \ / _` | '_ \ \___ \ / __| '__| | '_ \| __/ __|
+# | |\  | (_| | |_| | | | (_| | | | |____) | (__| |  | | |_) | |_\__ \
+# |_| \_|\__,_|\__|_| |_|\__,_|_| |_|_____/ \___|_|  |_| .__/ \__|___/
+#                                                      | |            
+#                                                      |_|            
+#
+#           Github : https://github.nathan-guillaume.be/
+#              Website : https://nathan-guillaume.be
+#
+#    This script is under development, it means that the script is not
+#               yet functional or contains bugs
+#
+#                         Language : French
+#                      Author : Nathan Guillame
+
+function update() {
+  apt-get update
+}
